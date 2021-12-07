f63235d95f79d1aa2f844e2db48bfcc62683f4df
Priscilla Wang <hjkim015@users.noreply.github.com>
2021-12-07 12:26:24 -0500

    update dashboard ui

--- helpers.py
+++ helpers.py
@@ -33,109 +33,101 @@ def check_registration(rows, username, password, confirmation):
 
 
 def matching_algorithm(mentee_id):
-    mentees = db.execute("SELECT id FROM users WHERE role = ?", (0, ))
-    mentors = db.execute("SELECT id FROM users WHERE role = ?", (1, ))
-    
-    # Count the number of mentors in the database
-    mentors = db.execute("SELECT id FROM users WHERE role = ?", (1, ))
-    print(len(mentors.fetchall()))
-    print("TEST")
-
-    #Count the number of mentors in the database
-    cmentors = db.execute("SELECT * FROM users WHERE role = ?", (1, )).fetchall()
-    print("TEST BEFORE", cmentors)
+    #Select all available mentors
+    cmentors = db.execute("SELECT person_id FROM mentee_count WHERE mentees_left > 0").fetchall()
+    print(cmentors)
     mentor_count = int(len(cmentors))
-    # print("mentor_count start", mentor_count)
-    cmentors = db.execute("SELECT * FROM users WHERE role = ?", (1, ))
-    mentor_count = cmentors.rowcount
-    status = 1
 
     # Compatibitility trackers store match score, mentee_id, and mentor_id
     oldcompatibility = [0,0,0]
-    newcompatibility = [0,0,0]
+    # newcompatibility = [0,0,0]
     mentee_id = str(mentee_id)
+
     print("start", oldcompatibility[0], oldcompatibility[1], oldcompatibility[2])
 
     # Store mentee's category weights 
-    academics = db.execute("SELECT academics FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()
-    print("academics is", academics)
-    gender = db.execute("SELECT gender FROM rankings WHERE person_id = ?", (mentee_id, ))
-    religion = db.execute("SELECT religion FROM rankings WHERE person_id = ?", (mentee_id, ))
-    ethnicity = db.execute("SELECT ethnicity FROM rankings WHERE person_id = ?", (mentee_id, ))
-    citizenship = db.execute("SELECT citzenship FROM rankings WHERE person_id = ?", (mentee_id, ))
-    
-    print("TEST AFTER", cmentors)
-    for mentor in cmentors:
-        print("INSIDE")
-        mentor = str(mentor)
-        #If there are mentors left, calculate scores 
-        if mentor_count > 0: 
-            #If mentor has mentee spots left, calculate match scores. 
-            if db.execute("SELECT mentees_left FROM mentee_count WHERE person_id = ?", (mentor, )) == 0:
-                mentor_count = mentor_count - 1
-            else:
-                #Set category match score to 1 if there is match and 0 if not 
-                academicsmatch1 = db.execute("SELECT academic_pathway FROM academics WHERE person_id = ?", (mentee_id,)).fetchall()
-                academicsmatch2 = db.execute("SELECT academic_pathway FROM academics WHERE person_id = ?", (mentor, )).fetchall()
-                
-                if academicsmatch1[0][1] == academicsmatch2[0][1]:
-                    academicsmatch = 1
-                else:
-                    academicsmatch = 0
+    academics = db.execute("SELECT academics FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()[0][0]
+    gender = db.execute("SELECT gender FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()[0][0]
+    religion = db.execute("SELECT religion FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()[0][0]
+    ethnicity = db.execute("SELECT ethnicity FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()[0][0]
+    citizenship = db.execute("SELECT citzenship FROM rankings WHERE person_id = ?", (mentee_id, )).fetchall()[0][0]
 
-                gendermatch = db.execute("SELECT gender FROM gender WHERE person_id = ? EXISTS (SELECT gender FROM gender WHERE person_id = ?)", (mentee_id, mentor, ))
-                if gendermatch == True:
-                    gendermatch = 1
-                else:
-                    gendermatch = 0
+    for mentor in cmentors: 
+        print("mentor_id, mentee_id", mentor[0], mentee_id)
+        mentor = str(mentor[0])
+        #If that mentor can take on more mentees
+        if mentor_count > 0:
+            match_score = 0
+            #Calcuate academics match
+            academicsmatch1 = db.execute("SELECT academic_pathway FROM academics WHERE person_id = ?", (mentee_id,)).fetchall()[0]
+            academicsmatch2 = db.execute("SELECT academic_pathway FROM academics WHERE person_id = ?", (mentor, )).fetchall()[0]
 
-                religionmatch = db.execute("SELECT religion FROM religion WHERE person_id = ? EXISTS (SELECT religion FROM religion WHERE person_id = ?)", (mentee_id, mentor, ))
-                if religionmatch == True:
-                    religionmatch = 1
-                else:
-                    religionmatch = 0
+            if academicsmatch1 == academicsmatch2:
+                academicsmatch = 1
+                match_score = match_score + academics
+                        
+            #Calculate Gender match
+            gendermatch1 = db.execute("SELECT gender FROM gender WHERE person_id = ?", (mentee_id, )).fetchall()[0]
+            gendermatch2 = db.execute("SELECT gender FROM gender WHERE person_id = ?", (mentor, )).fetchall()[0]
+            if gendermatch1 == gendermatch2:
+                match_score = match_score + gender
 
-                citizenshipmatch = db.execute("SELECT citizenship FROM citizenship WHERE person_id = ? EXISTS (SELECT citizenship FROM citizenship WHERE person_id = ?)", (mentee_id, mentor, ))
-                if citizenshipmatch == True:
-                    citizenshipmatch = 1
-                else:
-                    citizenshipmatch = 0
+            #Calculate Religion Match
+            religionmatch1 = db.execute("SELECT religion FROM religion WHERE person_id = ?", (mentee_id,)).fetchall()[0]
+            print(religionmatch1)
+            religionmatch2 = db.execute("SELECT religion FROM religion WHERE person_id = ?", (mentor,)).fetchall()[0]
+            print(religionmatch2)
+            if religionmatch1 == religionmatch2:
+                print("match_score, religion", match_score, religion)
+                match_score = match_score + religion
+            
 
-                # Ethnicity has a different process because of multi-select
-                # We will see if each option is checked or not in mentee and mentor side, then compute a correctness fraction
-                # The correctness fraction (called "ethnicitymatch") should be sum of all matched selection status of option/ total options
-                ethnicitymatch1 = db.execute("SELECT COUNT(black) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch2 = db.execute("SELECT COUNT(asian) FROM ethnicity WHERE person_id = ?) = (SELECT asian FROM ethnicity WHERE person_id = ?))", (mentee_id, mentor, )) + 1
-                ethnicitymatch3 = db.execute("SELECT COUNT(american_india) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch4 = db.execute("SELECT COUNT(pacific_islander) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch5 = db.execute("SELECT COUNT(white) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch6 = db.execute("SELECT COUNT(latinx) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch7 = db.execute("SELECT COUNT(none) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
-                ethnicitymatch = (ethnicitymatch1%2 + ethnicitymatch2%2 + ethnicitymatch3%2 + ethnicitymatch4%2 + ethnicitymatch5%2 + ethnicitymatch6%2 + ethnicitymatch7%2)/7
+            #Calculate Citizenship Match
+            citizenshipmatch1 = db.execute("SELECT citizenship FROM citizenship WHERE person_id = ?", (mentee_id, )).fetchall()[0]
+            citizenshipmatch2 = db.execute("SELECT citizenship FROM citizenship WHERE person_id = ?", (mentor, )).fetchall()[0]
+            if citizenshipmatch1 == citizenshipmatch2:
+                citizenshipmatch = 1
+                match_score = match_score + citizenship
 
-                # Compute a new compatibility score to store the current calculation
-                newcompatibility[0] = (academics * academicsmatch) + (religion * religionmatch) + (citizenship * citizenshipmatch) + (gender * gendermatch) + (ethnicity * ethnicitymatch)
-                
-                # If this new compatibility score is higher than that of the mentee's and the previous mentor, exchange the old compatibility with the new
-                # This way we ensure that at the end, the old compatibility is the highest found score combination for mentee and mentor pair
-                if newcompatibility[0] > oldcompatibility[0] or newcompatibility[0] == oldcompatibility[0]: 
-                    oldcompatibility[0] = newcompatibility[0]
-                    oldcompatibility[1] = mentee_id
-                    oldcompatibility[2] = mentor
+            # Calculate Ethnicity Match 
+            ethnicitymatch1 = db.execute("SELECT ethnicity FROM ethnicity WHERE person_id = ?", (mentee_id, )).fetchall()[0]
+            ethnicitymatch2 = db.execute("SELECT ethnicity FROM ethnicity WHERE person_id = ?", (mentor, )).fetchall()[0]
+            if ethnicitymatch1 == ethnicitymatch2:
+                match_score = match_score + ethnicity
+           
+            # ethnicitymatch1 = db.execute("SELECT COUNT(black) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch2 = db.execute("SELECT COUNT(asian) FROM ethnicity WHERE person_id = ?) = (SELECT asian FROM ethnicity WHERE person_id = ?))", (mentee_id, mentor, )) + 1
+            # ethnicitymatch3 = db.execute("SELECT COUNT(american_india) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch4 = db.execute("SELECT COUNT(pacific_islander) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch5 = db.execute("SELECT COUNT(white) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch6 = db.execute("SELECT COUNT(latinx) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch7 = db.execute("SELECT COUNT(none) FROM ethnicity WHERE person_id = ? OR person_id = ?", (mentee_id, mentor, )) + 1
+            # ethnicitymatch = (ethnicitymatch1%2 + ethnicitymatch2%2 + ethnicitymatch3%2 + ethnicitymatch4%2 + ethnicitymatch5%2 + ethnicitymatch6%2 + ethnicitymatch7%2)/7
+           
+            #Calculate the Overall Match Score 
+            # match_score = (academics * academicsmatch)[0] + (religion * religionmatch)[0] + (citizenship * citizenshipmatch)[0] + (gender * gendermatch)[0] + (ethnicity * ethnicitymatch)[0]
+            # print((academics * academicsmatch])
+           
+            #If the current Match Score is higher than the previous match score:
+            #Replace the old compatibility score with the current match score
+            # print("match_score", match_score)
+            # print("oldcompatibility[0]", oldcompatibility[0])
+            if match_score > oldcompatibility[0] or match_score == oldcompatibility[0]: 
+                oldcompatibility[0] = match_score
+                oldcompatibility[1] = mentee_id
+                oldcompatibility[2] = mentor
         else:
-            #otherwise, put mentees onto a waitlist
+            #Put the student on the waitlist -> 0 for mentor_id
             db.execute("INSERT INTO matches (score, mentee_id, mentor_id) VALUES (?,?,?)", (0, mentee_id, 0))
-            status = 0    
-       
-        # Insert highest score and match information into database
-        print("final thing to push to matches", oldcompatibility[0], oldcompatibility[1], oldcompatibility[2])
-        db.execute("INSERT INTO matches (score, mentee_id, mentor_id) VALUES (?,?,?)", (oldcompatibility[0], oldcompatibility[1], oldcompatibility[2], ))    
-        # Update the mentee count
-        # current_count = db.execute("SELECT mentees_left FROM mentee_count WHERE person_id = ?", (oldcompatibility[2], ))
-        # current_count = current_count.fetchall()[0] - 1
-        db.execute("UPDATE mentee_count SET mentees_left = mentees_left - 1 WHERE person_id = ?", (oldcompatibility[2], ))
-        data.commit()
+ 
+    #After going through all mentors, highest match score stored in old compatiability score
+    #Push to matches the old compatability information
+    db.execute("INSERT INTO matches (score, mentee_id, mentor_id) VALUES (?,?,?)", (oldcompatibility[0], oldcompatibility[1], oldcompatibility[2], ))    
+    print("final thing to push to matches", oldcompatibility[0], oldcompatibility[1], oldcompatibility[2])
 
+    #Decrease the number of mentees the mentor can take on by 1.
+    db.execute("UPDATE mentee_count SET mentees_left = mentees_left - 1 WHERE person_id = ?", (oldcompatibility[2], ))
+    data.commit()
 
-# matching_algorithm(109)
+# matching_algorithm(4)
     
